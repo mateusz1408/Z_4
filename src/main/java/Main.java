bb5d38aa6634b224e0451bbc7d7bea780d6d0954
mateusz1408 <162459899+mateusz1408@users.noreply.github.com>
2024-03-20 14:26:14 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int liczba1= scanner.nextInt();
+    int liczba2= scanner.nextInt();
+    int operacja= scanner.nextInt();
+
+    int wynik=0;
+
+    switch(operacja){
+      case 1:
+        wynik=liczba1+liczba2;
+        break;
+      case 2:
+        wynik=liczba1-liczba2;
+        break;
+      case 3:
+        wynik=liczba1*liczba2;
+        break;
+      case 4:
+        wynik=liczba1/liczba2;
+        break;
+        
+       }
+    System.out.println(wynik);
+  
+    
+  }
+
+  
+}
