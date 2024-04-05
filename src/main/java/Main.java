b439e417d54b8e9c6ed7d484908c320b486b189a
mateusz1408 <162459899+mateusz1408@users.noreply.github.com>
2024-04-05 08:58:26 +0000

    Z_4v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,31 +6,37 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int liczba1= scanner.nextInt();
-    int liczba2= scanner.nextInt();
-    int operacja= scanner.nextInt();
+    System.out.println("Podaj pierwszą liczbę:");
+    int liczba1 = scanner.nextInt();
+    System.out.println("Podaj drugą liczbę:");
+    int liczba2 = scanner.nextInt();
+    System.out.println("Wybierz operację: 1 - dodawanie, 2 - odejmowanie, 3 - mnożenie, 4 - dzielenie");
+    int operacja = scanner.nextInt();
 
-    int wynik=0;
+    int wynik = 0;
 
-    switch(operacja){
+    switch(operacja) {
       case 1:
-        wynik=liczba1+liczba2;
+        wynik = liczba1 + liczba2;
         break;
       case 2:
-        wynik=liczba1-liczba2;
+        wynik = liczba1 - liczba2;
         break;
       case 3:
-        wynik=liczba1*liczba2;
+        wynik = liczba1 * liczba2;
         break;
       case 4:
-        wynik=liczba1/liczba2;
+        if (liczba2 != 0) {
+          wynik = liczba1 / liczba2;
+        } else {
+          System.out.println("Błąd: Dzielenie przez zero.");
+          scanner.close();
+          return; 
+        }
         break;
-        
-       }
-    System.out.println("wynik to " + wynik);
-  
-    
-  }
+    }
 
-  
+    System.out.println("Wynik to " + wynik);
+    scanner.close();
+  }
 }
